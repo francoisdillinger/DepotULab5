cd254334e6ff697ea3a13ec8618c3537c169506c
James Jester <elmojester@gmail.com>
2016-07-06 15:14:45 -0500

    Cleaning up code.

--- jquery.js
+++ jquery.js
@@ -12,13 +12,15 @@
                                 'oat itain oat tain nate eate tea anne inant nean',
                                  'itant eate anot eat nato inate eat anot tain eat', 
                                 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+          
+            var lineForScreen = sentences[0];
             var correct = 0;
             var wrong = 0;
-            // var endingMessage = 'Congrats! You got ' + correct + ' correct and ' + wrong + ' wrong. Would you like to play again?';
+           
                               
    // Posting the first sentence to the top of the screen. 
                
-            $('#words').text(sentences[0]);   
+            $('#words').text(lineForScreen);   
                
     // Function that will register a key being pressed and .which will record the keycode of key pressed.    For this function the SHIFT key will show the uppercase keyboard. 
            
@@ -43,47 +45,57 @@
                 var theKey = '#' + grr.keyCode;
                 var theLetter = String.fromCharCode(grr.keyCode);
                 $(theKey).addClass('right');
-                // console.log(grr.keyCode);
+                
                         
                         console.log(sentences.length);
                     if(sentences.length >= 1){
-                        if(sentences[0].length){
+                        if(sentences[0].length >= 1){
                             var singleLetter = sentences[0].slice(0,1);
-                            // console.log(theSentence);
+                           
                             var theSentence = sentences.shift();
+                          
                             var newSentence = theSentence.substring(1);
-                            // theSentence = theSentence.substring(1);
+                            lineForScreen = newSentence;
+                        
                             sentences.unshift(newSentence);
-                            // console.log(theLetter);
-                            // console.log(singleLetter);
+                          
+                            $('#words-typed').text(theLetter);
                             if(theLetter === singleLetter){
                                 console.log('Match!');
-                                correct = correct + 1;
-                                console.log(correct);
+                                correct++;
+                                
+                                $('#words').text(lineForScreen);
+                                $('#icons').html('<span class="glyphicon glyphicon-ok"></span>');
+                                
                             }
                             else{
                                 console.log('Wrong');
-                                wrong = wrong + 1;
-                                console.log(wrong);
+                                wrong++;
+                                
+                                $('#words').text(lineForScreen);
+                                $('#icons').html('<span class="glyphicon glyphicon-remove"></span>');
                             }
-                            console.log(sentences.length);
+                          
                         }
                         else{
+                            $('#words').text(sentences[1]);
                                 console.log('Im shifting!');
                                 sentences.shift();
+
                         }
                     }
                     else{
                         var endingMessage = 'Congrats! You got ' + correct + ' correct and ' + wrong + ' wrong. Would you like to play again?';
                         alert(endingMessage);
-                        console.log(endingMessage);
+                        
                     }
-                    console.log(sentences);
+                   
             });
             
       // Similar to the SHIFT, upon keyup it removes the #right class, which then removes the highlight that the key recieves once its pressed down.       
             $(document).keyup(function (grr) {
-                $('.well').removeClass('right'); 
+                $('.well').removeClass('right');
+                 $('#icons').empty();
                 $('#words-typed').empty();
                 
                 });
